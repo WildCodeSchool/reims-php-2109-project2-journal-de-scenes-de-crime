c6186d6e5750764cc99cf0b166b0fb944a86d743
HofmannAlexandre <alexandre.hofmannwild@gmail.com>
2021-10-27 17:07:32 +0200

    add fonction update and delete for CrimeSceneManager.php

--- src/Model/CrimeSceneManager.php
+++ src/Model/CrimeSceneManager.php
@@ -4,42 +4,54 @@ namespace App\Model;
 
 class CrimeSceneManager extends AbstractManager
 {
-        public const TABLE = 'crime_scene';
-    
+    public const TABLE = 'crime_scene';
+
         /**
-         * Show crime scene in database
-         */
-        public function show()
-        {
-            $statement = $this->pdo->prepare("SELECT name, title, adress, description, date, victim FROM . self::TABLE");
-            return $statement->execute();
-        }
+        * Show crime scene in database
+        */
+    public function show()
+    {
+        $statement = $this->pdo->prepare("SELECT name, title, adress, description, date, victim 
+        FROM crime_scene");
+
+            return $statement->fetch();
+    }
         /**
-         * Insert new item in database
+         * Insert new crime scene in database
          */
-        public function insert(array $crime_scene): string
-        {
-            $statement = $this->pdo->prepare("INSERT INTO " . self::TABLE . " (`name, title, adress, description, date, victim`) VALUES (:name, :title, :adress, :description, :date, :victime)");
-            $statement->bindValue('name', $crime_scene['name'], \PDO::PARAM_STR);
-            $statement->bindValue('title', $crime_scene['title'], \PDO::PARAM_STR);
-            $statement->bindValue('adress', $crime_scene['adress'], \PDO::PARAM_STR);
-            $statement->bindValue('description', $crime_scene['description'], \PDO::PARAM_STR);
-            $statement->bindValue('date', $crime_scene['date'], \PDO::PARAM_STR);
-            $statement->bindValue('victim', $crime_scene['victim'], \PDO::PARAM_STR);
-    
-            return $statement->execute();
-        }
-    
+    public function insert(array $crimeScene)
+    {
+        $statement = $this->pdo->prepare("INSERT INTO " . self::TABLE . " (`name, title, adress, 
+        description, date, victim`) VALUES (:name, :title, :adress, :description, :date, :victime)");
+        $statement->bindValue('name', $crimeScene['name'], \PDO::PARAM_STR);
+        $statement->bindValue('title', $crimeScene['title'], \PDO::PARAM_STR);
+        $statement->bindValue('adress', $crimeScene['adress'], \PDO::PARAM_STR);
+        $statement->bindValue('description', $crimeScene['description'], \PDO::PARAM_STR);
+        $statement->bindValue('date', $crimeScene['date'], \PDO::PARAM_STR);
+        $statement->bindValue('victim', $crimeScene['victim'], \PDO::PARAM_STR);
+
+        return $statement->execute();
+    }
+
         /**
-         * Update item in database
+         * Update crime scene in database
          */
-        public function update(array $item): bool
-        {
-            $statement = $this->pdo->prepare("UPDATE " . self::TABLE . " SET `title` = :title WHERE id=:id");
-            $statement->bindValue('id', $item['id'], \PDO::PARAM_INT);
-            $statement->bindValue('title', $item['title'], \PDO::PARAM_STR);
-    
-            return $statement->execute();
-        }
-    
+    public function update(array $crimeScene)
+    {
+        $statement = $this->pdo->prepare("UPDATE " . self::TABLE . " SET `title, description` = :title, :description, WHERE id=:id");
+        $statement->bindValue('id', $crimeScene['id'], \PDO::PARAM_INT);
+        $statement->bindValue('title', $crimeScene['title'], \PDO::PARAM_STR);
+        $statement->bindValue('description', $crimeScene['description'], \PDO::PARAM_STR);
+
+        return $statement->execute();
+    }
+        /**
+        * Delete row form an ID
+        */
+    public function delete(int $id): void
+    {    
+        $statement = $this->pdo->prepare("DELETE FROM " . static::TABLE . " WHERE id=:id");
+        $statement->bindValue('id', $id, \PDO::PARAM_INT);
+        $statement->execute();
+    }
 }

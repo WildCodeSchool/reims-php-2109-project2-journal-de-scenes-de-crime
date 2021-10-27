a040f0d015e594134bf7dbb35a5ea66f6b9f93d0
noeliedubos <noelie.dubos@gmail.com>
2021-10-27 17:33:39 +0200

    i create aan insertion in the database for testing

---
+++ insertStory.sql
@@ -0,0 +1,15 @@
+INSERT INTO `crime_scene` (
+  `name`,
+  `title`,
+  `adress`,
+  `description`,
+  `date`,
+  `victim`
+  VALUES
+  `Watson`,
+  `Le mysthère de la chambre rouge`,
+  `3, Lauriston Gardens, Londre`,
+  `C’était le cadavre d’un homme d’environ quarante-trois, de taille moyenne, large d’épaules, avec des cheveux noirs et une barbe de trois jours. Ses mains étaient crispées et ses bras étendus. L’agonie avait dû être douloureuse ! Son visage rigide conservait une expression d’horreur.`,
+  `name`,
+  `le cadavre d’Enoch J. Drebber`,
+);

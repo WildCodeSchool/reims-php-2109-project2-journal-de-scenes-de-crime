7ef9c88e262c0e3a833be0246e249ba1f3f3fde5
noeliedubos <noelie.dubos@gmail.com>
2021-10-28 10:11:11 +0200

    i change the file

--- insertStory.sql
+++ insertStory.sql
@@ -1,15 +1,17 @@
-INSERT INTO `crime_scene` (
-  `name`,
-  `title`,
-  `adress`,
-  `description`,
-  `date`,
-  `victim`
-  VALUES
-  `Watson`,
-  `Le mysthère de la chambre rouge`,
-  `3, Lauriston Gardens, Londre`,
-  `C’était le cadavre d’un homme d’environ quarante-trois, de taille moyenne, large d’épaules, avec des cheveux noirs et une barbe de trois jours. Ses mains étaient crispées et ses bras étendus. L’agonie avait dû être douloureuse ! Son visage rigide conservait une expression d’horreur.`,
-  `name`,
-  `le cadavre d’Enoch J. Drebber`,
-);
+ALTER TABLE `crime_scene` CHANGE description description TEXT;
+INSERT INTO crime_scene 
+    (
+    name,
+    title,
+    adress,
+    description,
+    date,
+    victim) 
+    VALUES 
+    (
+    "Watson",
+    "Le mysthère de la chambre rouge",
+    "3 Lauriston Gardens Londre",
+    "C’était le cadavre d’un homme d’environ quarante-trois, de taille moyenne,avec des cheveux noirs et une barbe de trois jours. L’agonie avait dû être douloureuse ! Son visage rigide conservait une expression d’horreur.",
+    "2021-01-01",
+    "le cadavre d’Enoch J. Drebber");

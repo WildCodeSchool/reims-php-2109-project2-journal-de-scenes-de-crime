0eceb4ae005eaa49f061d6801bfab85fbfcfa1c5
CorentinChalons <chalons.corentin@gmail.com>
2021-11-03 11:12:41 +0100

    Modification form, modification function insert in manager and add new function in crimescenecontroller

--- src/Controller/CrimeSceneController.php
+++ src/Controller/CrimeSceneController.php
@@ -14,8 +14,23 @@ class CrimeSceneController extends AbstractController
         return $this->twig->render('Home/index.html.twig', ['titles' => $titles]);
     }
 
+    /**
+     * Add a new item
+     */
     public function add(): string
     {
+        $crimeSceneManager = new CrimeSceneManager();
+
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            // clean $_POST data
+            $crimeScene = array_map('trim', $_POST);
+
+            // TODO validations (length, format...)
+
+            // if validation is ok, insert and redirection
+            $crimeSceneManager->insert($crimeScene);
+        }
+
         return $this->twig->render('CreateCrime/CreateYourCrime.html.twig');
     }
 }

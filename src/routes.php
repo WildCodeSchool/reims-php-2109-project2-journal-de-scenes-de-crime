b04bf221184037829e4ec97e80eb277858eb0a6c
CorentinChalons <chalons.corentin@gmail.com>
2021-11-03 09:40:52 +0100

    Link home page to create crime scene and link for back to home page

--- src/routes.php
+++ src/routes.php
@@ -11,6 +11,6 @@ return [
     'items' => ['ItemController', 'index',],
     'items/edit' => ['ItemController', 'edit', ['id']],
     'items/show' => ['ItemController', 'show', ['id']],
-    'items/add' => ['ItemController', 'add',],
+    'crime/add' => ['CrimeSceneController', 'add',],
     'items/delete' => ['ItemController', 'delete',],
 ];
